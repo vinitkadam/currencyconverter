ee0a2da00174b182d88fc56b1afa0612f1f37b78
vinitkadam <vinitkadam1997@gmail.com>
2018-04-23 02:22:18 +0530

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -45,16 +45,3 @@ class MyServer{
 		ss.close();  
 	}
 }
-
-// String sURL = "http://freegeoip.net/json/"; //just a string
-
-//     // Connect to the URL using java's native library
-//     URL url = new URL(sURL);
-//     HttpURLConnection request = (HttpURLConnection) url.openConnection();
-//     request.connect();
-
-//     // Convert to a JSON object to print data
-//     JsonParser jp = new JsonParser(); //from gson
-//     JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-//     JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object. 
-//     zipcode = rootobj.get("zip_code").getAsString();
